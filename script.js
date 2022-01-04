9a31d0a8a79eff6b48a0fa01a6a5fc098c925bdd
RUTURAJ KALSHETTI <65947291+anonymousCoder04@users.noreply.github.com>
2022-01-04 20:29:05 +0530

    added meta data loader

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 $(document).ready(function () {
   const songs = [
-    "2U_Justin_Bieber",
-    "Alan_Walker_All_Falls_Down",
-    "Alan_Walker_Alone",
-    "Alan_Walker_Ava_Max_Alone_Pt_II",
-    "Alan_Walker_Darkside",
+    "alone pt 2",
+    "crowd control",
+    "fake a smile",
+    "unity",
+    "warriyo mortals",
   ];
+
+  const loadMetadata = () => {
+    jsmediatags.read(`http://127.0.0.1:5500/songs/${songs[iter]}.mp3`, {
+      onSuccess: function (tag) {
+        let picture = tag.tags.picture;
+        var base64String = "";
+        for (var i = 0; i < picture.data.length; i++) {
+          base64String += String.fromCharCode(picture.data[i]);
+        }
+        var imageUri =
+          "data:" + picture.format + ";base64," + window.btoa(base64String);
+        $(".cover-art").attr("src", imageUri);
+        $("#np-title").text(tag.tags.title);
+        $("#np-artist").text(tag.tags.artist);
+        console.log(tag.tags.album, tag.tags.artist, tag);
+      },
+      onError: function (error) {
+        console.log(error);
+      },
+    });
+  };
 
   const guardFlow = () => {
     iter < songs.length - 1
@@ -15,13 +36,13 @@ $(document).ready(function () {
       ? $("#skipbck").prop("disabled", false)
       : $("#skipbck").prop("disabled", true);
   };
+
   const timeFuncs = () => {
     let playtime;
-    $("#track-time").text(
-      `${Math.floor(player.duration / 60)}:${
-        Math.round(player.duration) - 60 * Math.floor(player.duration / 60)
-      }`
-    );
+    let tracktime =
+      Math.round(player.duration) - 60 * Math.floor(player.duration / 60);
+    tracktime = tracktime > 10 ? tracktime : "0" + tracktime.toString();
+    $("#track-time").text(`${Math.floor(player.duration / 60)}:${tracktime}`);
 
     player.ontimeupdate = () => {
       $(".tracker").width(`${(player.currentTime * 100) / player.duration}%`);
@@ -47,8 +68,8 @@ $(document).ready(function () {
     };
     console.log(player.duration, player);
   };
-  let iter = 0;
-  let player = new Audio(`./songs/${songs[0]}.mp3`);
+  let iter = 1;
+  let player = new Audio(`./songs/${songs[iter]}.mp3`);
   // let player = new Audio(`./songs/${songs[0]}.mp3`);
   console.log("Jquery Ready");
 
@@ -65,12 +86,12 @@ $(document).ready(function () {
   $(".now-playing").click(function () {
     $(this).addClass("fscreen");
   });
-
+  player.currentTime = 180;
   $("#collapse-fscreen").click(function (e) {
     e.stopPropagation();
     $(".now-playing").removeClass("fscreen");
   });
-
+  loadMetadata();
   $("#playbtn.controller").click(function (e) {
     e.stopPropagation();
     if ($(".play.paused").length == 0) {
@@ -96,6 +117,7 @@ $(document).ready(function () {
       player.play();
       $(".play").removeClass("paused");
       timeFuncs();
+      loadMetadata();
     }, 750);
   });
   $("#skipbck").click(function (e) {
@@ -110,6 +132,7 @@ $(document).ready(function () {
       player.play();
       $(".play").removeClass("paused");
       timeFuncs();
+      loadMetadata();
     }, 750);
   });
 
@@ -164,53 +187,16 @@ $(document).ready(function () {
       })
       .removeClass("isactive");
   });
-  // Profile View
-  $('#edit-info').click(function(){
-    console.log('hi');
-      if($("#edit-info.active").length == 0){
-          $(this).addClass('active');
-          $('.profile.view.viewing input').attr('disabled', false);
-      }
-      else{
-        $(this).removeClass('active');
-        $('.profile.view.viewing input').attr('disabled', true);
 
-      }
-  })
-});
-/*
-This is code jor jsmediatag reader
-let jstagreader = new jsmediatags.Reader();
-  console.log(jstagreader);
-  jstagreader._file =
-    "http://localhost:8080/songs/Alan_Walker_Ava_Max_Alone_Pt_II.mp3";
-  jstagreader.read({
-    onSuccess: (tag) => {
-      console.log("Success!");
-      console.log(tag);
-    },
-    onError: (error) => {
-      console.log("Error");
-      console.log(error);
-    },
-  });
-  jsmediatags.read(
-    "http://127.0.0.1:5500/songs/Alan_Walker_Ava_Max_Alone_Pt_II.mp3",
-    {
-      onSuccess: function (tag) {
-        let picture = tag.tags.picture;
-        var base64String = "";
-        for (var i = 0; i < picture.data.length; i++) {
-          base64String += String.fromCharCode(picture.data[i]);
-        }
-        var imageUri =
-          "data:" + picture.format + ";base64," + window.btoa(base64String);
-        $(".cover-art").attr("src", imageUri);
-        console.log(imageUri);
-      },
-      onError: function (error) {
-        console.log(error);
-      },
+  // Profile View
+  $("#edit-info").click(function () {
+    console.log("hi");
+    if ($("#edit-info.active").length == 0) {
+      $(this).addClass("active");
+      $(".profile.view.viewing input").attr("disabled", false);
+    } else {
+      $(this).removeClass("active");
+      $(".profile.view.viewing input").attr("disabled", true);
     }
-  );
- */
+  });
+});
