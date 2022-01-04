b37e1efc47ed3a1a11a08db0ede32d6e64038d73
RUTURAJ KALSHETTI <65947291+anonymousCoder04@users.noreply.github.com>
2022-01-04 20:28:39 +0530

    temp file

--- templates.js
+++ templates.js
@@ -1,1 +1,8 @@
 //this will have templates here which will be inserted into html via javascript
+const fs = require("fs");
+const path = require("path");
+fs.readdir(path.resolve(__dirname, "songs"), (err, files) => {
+  files.forEach((file) => {
+    console.log(file);
+  });
+});
